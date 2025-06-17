1a1a5e9c9007e3f22df373dbaeddb143bcbaba25
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-17 21:55:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed = topSpeed;
+}
+SportsCar.prototype = Object.create(Car.prototype);
 
-function SportsCar(make, model, topSpeed) {}
+// Reset the constructor property
+SportsCar.prototype.constructor = SportsCar;
+
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
